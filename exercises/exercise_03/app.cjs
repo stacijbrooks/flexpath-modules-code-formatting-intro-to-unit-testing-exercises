328e73f4ce7b1c0f246391fdf64f60c6a6413d87
Staci Brooks <stacibrooksfitness@gmail.com>
2026-06-10 22:09:30 -0500

    import and export Koda

---
+++ exercises/exercise_03/app.cjs
@@ -0,0 +1,6 @@
+//Import functions from stringUtils.cjs
+const { toUpperCase, toLowerCase } = require('./stringUtils.cjs');
+
+//Call the functions and print
+console.log(toUpperCase('koda'));
+console.log(toLowerCase('KODA'));
---
+++ exercises/exercise_03/stringUtils.cjs
@@ -0,0 +1,15 @@
+//Converts a string to ALL UPPERCASE
+function toUpperCase(str) {
+    return str.toUpperCase();
+}
+
+//Converts string to Lowercase
+function toLowerCase(str) {
+    return str.toLowerCase();
+}
+
+//Export both functions
+module.exports = {
+    toUpperCase,
+    toLowerCase
+};
