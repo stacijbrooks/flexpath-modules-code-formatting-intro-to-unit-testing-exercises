e89b95b1f87fd0d9216472012a63545a650f7b12
Staci Brooks <stacibrooksfitness@gmail.com>
2026-06-10 21:57:11 -0500

    Completed Modules 1 and 2

---
+++ exercises/exercise_01_and_02/app.js
@@ -0,0 +1,4 @@
+import {add, multiply} from './mathUtils.js';
+
+console.log(add(5,3));
+console.log(multiply(5,3));
---
+++ exercises/exercise_01_and_02/mathUtils.js
@@ -0,0 +1,11 @@
+//Adds two numbers
+export function add(a, b) {
+    return a + b;
+}
+
+//Multiplies two numbers
+export function multiply(a,b) {
+    return a * b;
+}
+
+//export function add(a,b);
